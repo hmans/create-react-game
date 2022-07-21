c495b20c92844b90935721982cb75707b35a7c27
Hendrik Mans <hendrik@mans.de>
2022-07-21 20:15:13 +0200

    Add a blank Vite app

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 #!/usr/bin/env node
-console.log("Hi from create-react-game!");
+import fs from "node:fs"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import minimist from "minimist"
+
+console.log("Hi from create-react-game")
+
+const templateDir = "./template-react-game"
+
+const files = fs.readdirSync(templateDir)
+for (const file of files.filter((f) => f !== "package.json")) {
+	// write(file)
+	console.log(file)
+}
