2f129d0a328f6d823c7c731e8bf514c7c499081c
Hendrik Mans <hendrik@mans.de>
2022-07-21 20:29:58 +0200

    Get copyin'

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 #!/usr/bin/env node
 import fs from "node:fs"
+import fse from "fs-extra"
+
 import path from "node:path"
 import { fileURLToPath } from "node:url"
 import minimist from "minimist"
+import prompts from "prompts"
+import { red } from "kolorist"
 
 console.log("Hi from create-react-game")
+var argv = minimist(process.argv.slice(2))
 
-const templateDir = "./template-react-game"
+async function install() {
+	const templateDir = "./template-react-game"
 
-const files = fs.readdirSync(templateDir)
-for (const file of files.filter((f) => f !== "package.json")) {
-	// write(file)
-	console.log(file)
+	const { targetDir } = await prompts({
+		type: "text",
+		name: "targetDir",
+		message: "Waagh?",
+		initial: argv._[0]
+	})
+
+	console.log(red(`Copying files, woohoo!`))
+	fse.copySync(templateDir, targetDir, {})
 }
+
+await install()
