2814eaaa11016f274f57e3f7cbcf4c59df9b8439
Hendrik Mans <hendrik@mans.de>
2022-07-22 08:51:09 +0200

    Use degit

--- index.js
+++ index.js
@@ -3,39 +3,16 @@ import fs from "node:fs"
 import fse from "fs-extra"
 
 import path from "node:path"
-import { fileURLToPath } from "node:url"
 import minimist from "minimist"
 import prompts from "prompts"
 import { green, red } from "kolorist"
 
-function copyFolderSync(from, to) {
-	/* Abort if target directory already exists. */
-	if (fs.existsSync(to)) {
-		console.error(red("Target directory already exists, aborting"))
-		return
-	}
-
-	/* Create target directory */
-	fse.mkdirpSync(to)
-
-	/* Recursively copy all template files */
-	fs.readdirSync(from).forEach((element) => {
-		console.log(`💾 ${element}`)
-
-		if (fs.lstatSync(path.join(from, element)).isFile()) {
-			fs.copyFileSync(path.join(from, element), path.join(to, element))
-		} else {
-			copyFolderSync(path.join(from, element), path.join(to, element))
-		}
-	})
-}
+import degit from "degit"
 
 console.log("Hi from create-react-game")
 var argv = minimist(process.argv.slice(2))
 
 async function install() {
-	const templateDir = "./template-react-game"
-
 	const { targetDir } = await prompts({
 		type: "text",
 		name: "targetDir",
@@ -44,7 +21,20 @@ async function install() {
 	})
 
 	console.log(green(`Copying files, woohoo!`))
-	copyFolderSync(templateDir, targetDir)
+
+	const emitter = degit("hmans/create-react-game/template-react-game", {
+		cache: false,
+		force: false,
+		verbose: false
+	})
+
+	emitter.on("info", (info) => {
+		// console.log(info.message)
+	})
+
+	await emitter.clone(targetDir)
+
+	console.log(green`Done!`)
 }
 
 await install()
